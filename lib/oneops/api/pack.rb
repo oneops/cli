77a1929e5a43b03fc76e97cfbbb05024e3fba14d
Lev Khusid <lkhusid@gmail.com>
2016-07-26 00:03:52 -0400

    Updated "design packs" to use new endpoint and not require assembly parameter.

--- lib/oneops/api/pack.rb
+++ lib/oneops/api/pack.rb
@@ -1,7 +1,7 @@
 class OO::Api::Pack < OO::Api::Base
   qualifiers :source
   def self.all
-    ok, data = request(:get, 'packs')
+    ok, data = request(:get, 'catalog/packs')
     return ok ? data.map {|source, pack| new(source, pack)} : []
   end
 end
--- lib/oneops/version.rb
+++ lib/oneops/version.rb
@@ -1,4 +1,4 @@
 module OO
   # This version number is used as the RubyGem release version.
-  VERSION = '0.3.1'
+  VERSION = '0.3.2'
 end
